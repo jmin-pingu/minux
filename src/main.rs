2e0167a056dd9865b6ac5bda22a358ca38fc02fc
jmin111301 <jonathanheromin@gmail.com>
2024-07-24 16:45:50 -0400

    Base Rust kernel operational

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+fn main() { }
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
+
